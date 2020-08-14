e83fe81edf35cdb3886f73ac75d2b19c10dbe471
nessie <nessie@hotmail.dk>
2020-08-14 23:34:37 +0200

    Make it so recipe book shows craftables by default.

---
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinRecipeBook.java
@@ -0,0 +1,19 @@
+package net.dugged.cutelessmod.mixins;
+
+import net.minecraft.stats.RecipeBook;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RecipeBook.class)
+public abstract class MixinRecipeBook {
+	@Shadow
+	protected boolean isFilteringCraftable;
+
+	@Inject(method = "<init>", at = @At("HEAD"))
+	private void onInit(CallbackInfo ci) {
+		this.isFilteringCraftable = true;
+	}
+}
