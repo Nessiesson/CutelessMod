09587bcf984e4c11c57d46ae6f93b28da08c9fb6
robitobi01 <robitobi01@gmx.de>
2020-08-18 21:31:26 +0200

    clean code up a little bit

--- src/main/java/net/dugged/cutelessmod/mixins/MixinGuiIngame.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinGuiIngame.java
@@ -1,7 +1,6 @@
 package net.dugged.cutelessmod.mixins;
 
 import net.dugged.cutelessmod.CutelessMod;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiIngame;
 import org.apache.commons.lang3.StringUtils;
@@ -18,8 +17,7 @@ public abstract class MixinGuiIngame extends Gui {
 		if (message.matches("\u00A7a\\d*\u00A7r") && StringUtils.isNumeric(s)) {
 			CutelessMod.overlayTimer = 60;
 			CutelessMod.mspt = Integer.parseInt(s);
-		} else {
-			Minecraft.getMinecraft().ingameGUI.setOverlayMessage(message, animateColor);
+			ci.cancel();
 		}
 	}
 }
