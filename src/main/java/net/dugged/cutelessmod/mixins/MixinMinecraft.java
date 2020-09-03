ed5e4f8013a034afd73aaeeab2cf09ec1b9c9708
nessie <nessie@hotmail.dk>
2020-09-03 20:46:15 +0200

    Make alwaysPickBlockMaxStack not require any special per-vanilla/forge code.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinMinecraft.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinMinecraft.java
@@ -2,8 +2,14 @@ package net.dugged.cutelessmod.mixins;
 
 import net.dugged.cutelessmod.Configuration;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.profiler.ISnooperInfo;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.IThreadListener;
+import net.minecraft.util.math.RayTraceResult;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,6 +20,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft implements IThreadListener, ISnooperInfo {
+	@Shadow
+	public EntityPlayerSP player;
+	@Shadow
+	public PlayerControllerMP playerController;
+	@Shadow
+	public RayTraceResult objectMouseOver;
 	@Shadow
 	private int rightClickDelayTimer;
 
@@ -30,5 +42,16 @@ public abstract class MixinMinecraft implements IThreadListener, ISnooperInfo {
 	@ModifyArg(method = "runTickMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(FFF)F"), index = 2)
 	private float allowFasterSpectatorFlight(float value) {
 		return (float) Configuration.spectatorMaxSpeed;
+	}
+
+	@Inject(method = "middleClickMouse", at = @At("RETURN"))
+	private void onMiddleClickMouse(final CallbackInfo ci) {
+		if (Configuration.alwaysPickBlockMaxStack && this.player.isCreative() && this.objectMouseOver != null && this.objectMouseOver.typeOfHit != RayTraceResult.Type.MISS) {
+			final InventoryPlayer inv = this.player.inventory;
+			final ItemStack currentItem = inv.getCurrentItem();
+			currentItem.setCount(currentItem.getMaxStackSize());
+			inv.setPickedItemStack(currentItem);
+			this.playerController.sendSlotPacket(this.player.getHeldItem(EnumHand.MAIN_HAND), 36 + inv.currentItem);
+		}
 	}
 }
