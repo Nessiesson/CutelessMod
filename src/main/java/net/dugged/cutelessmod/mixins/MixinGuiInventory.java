6e651c9f4c01bcb05f53245dafae2e5dbed2244e
nessie <nessie@hotmail.dk>
2020-10-03 06:46:47 +0200

    Apply `@Unique` annotations and `final` modifiers to some random eligable targets in the MixinGuiInventory mixin target class.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinGuiInventory.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinGuiInventory.java
@@ -12,44 +12,47 @@ import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiInventory.class)
 public abstract class MixinGuiInventory extends InventoryEffectRenderer {
-	private static final ResourceLocation POTION_BUTTON = new ResourceLocation(Reference.MODID, "textures/potion_button.png");
 	@Shadow
 	private GuiButtonImage recipeButton;
 	@Shadow
 	@Final
 	private GuiRecipeBook recipeBookGui;
-	private boolean showPotionEffects = false;
+	@Unique
+	private static final ResourceLocation CUTELESSMOD_POTION_BUTTON = new ResourceLocation(Reference.MODID, "textures/potion_button.png");
+	@Unique
+	private boolean cutelessModShowPotionEffects = false;
 
-	public MixinGuiInventory(Container inventorySlotsIn) {
-		super(inventorySlotsIn);
+	public MixinGuiInventory(final Container container) {
+		super(container);
 	}
 
 	@Inject(method = "drawScreen", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/inventory/GuiInventory;recipeBookGui:Lnet/minecraft/client/gui/recipebook/GuiRecipeBook;", ordinal = 1))
-	private void drawScreen(CallbackInfo ci) {
-		this.hasActivePotionEffects = !this.recipeBookGui.isVisible() && showPotionEffects;
-		if (GuiScreen.isShiftKeyDown() && recipeButton.isMouseOver()) {
-			((IGuiButtonImage) recipeButton).setResourceLocation(POTION_BUTTON);
-			((IGuiButtonImage) recipeButton).setXTexStart(0);
-			((IGuiButtonImage) recipeButton).setYDiffText(0);
+	private void drawScreen(final CallbackInfo ci) {
+		this.hasActivePotionEffects = !this.recipeBookGui.isVisible() && cutelessModShowPotionEffects;
+		final IGuiButtonImage button = (IGuiButtonImage) this.recipeButton;
+		if (GuiScreen.isShiftKeyDown() && this.recipeButton.isMouseOver()) {
+			button.setResourceLocation(CUTELESSMOD_POTION_BUTTON);
+			button.setXTexStart(0);
+			button.setYDiffText(0);
 		} else {
-			((IGuiButtonImage) recipeButton).setResourceLocation(INVENTORY_BACKGROUND);
-			((IGuiButtonImage) recipeButton).setXTexStart(178);
-			((IGuiButtonImage) recipeButton).setYDiffText(19);
+			button.setResourceLocation(INVENTORY_BACKGROUND);
+			button.setXTexStart(178);
+			button.setYDiffText(19);
 		}
 	}
 
 	@Inject(method = "actionPerformed", at = @At("HEAD"), cancellable = true)
-	private void actionPerformed(GuiButton button, CallbackInfo ci) {
-		if (GuiScreen.isShiftKeyDown() && recipeButton.isMouseOver() && button.id == 10 && !this.recipeBookGui.isVisible()) {
-			showPotionEffects = !showPotionEffects;
+	private void actionPerformed(final GuiButton button, final CallbackInfo ci) {
+		if (GuiScreen.isShiftKeyDown() && this.recipeButton.isMouseOver() && button.id == 10 && !this.recipeBookGui.isVisible()) {
+			this.cutelessModShowPotionEffects = !this.cutelessModShowPotionEffects;
 			ci.cancel();
 		}
 	}
-
 }
