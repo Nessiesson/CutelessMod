083d553c03aec6d54cf9a3020904dcafe180af2c
nessie <nessie@hotmail.dk>
2020-11-02 02:14:45 +0100

    Fix region overlay not working in F3 menu.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinGuiOverlayDebug.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinGuiOverlayDebug.java
@@ -3,11 +3,8 @@ package net.dugged.cutelessmod.mixins;
 import net.dugged.cutelessmod.CutelessMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiOverlayDebug;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -18,10 +15,6 @@ import java.util.List;
 
 @Mixin(GuiOverlayDebug.class)
 public abstract class MixinGuiOverlayDebug {
-	@Shadow
-	@Final
-	private Minecraft mc;
-
 	@Inject(method = "call", at = @At("RETURN"))
 	private void packetCount(final CallbackInfoReturnable<List<String>> cir) {
 		final int rx_per_second = Arrays.stream(CutelessMod.receivedPackets).sum();
@@ -31,12 +24,7 @@ public abstract class MixinGuiOverlayDebug {
 
 	@Inject(method = "call", at = @At("RETURN"))
 	private void displayRegion(final CallbackInfoReturnable<List<String>> cir) {
-		final Entity entity = this.mc.getRenderViewEntity();
-		if (entity == null || entity.getCollisionBoundingBox() == null) {
-			return;
-		}
-
-		final BlockPos pos = new BlockPos(entity.posX, entity.getCollisionBoundingBox().minY, entity.posZ);
+		final BlockPos pos = Minecraft.getMinecraft().player.getPosition();
 		final String region = "r." + (pos.getX() >> 9) + "." + (pos.getZ() >> 9) + ".mca";
 		cir.getReturnValue().add(11, String.format("Region: %s", region));
 	}
