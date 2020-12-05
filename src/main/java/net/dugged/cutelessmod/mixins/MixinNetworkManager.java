75effe33fef34c9f5c74de90fc386d8c088dc42a
nessie <nessie@hotmail.dk>
2020-12-05 16:55:37 +0100

    Avoid getting disconnected when receiving abnormally large packets.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinNetworkManager.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinNetworkManager.java
@@ -1,5 +1,7 @@
 package net.dugged.cutelessmod.mixins;
 
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.DecoderException;
 import net.dugged.cutelessmod.CutelessMod;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
@@ -13,5 +15,12 @@ public abstract class MixinNetworkManager {
 	@Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At("HEAD"))
 	private void countSendPackets(final Packet<?> packetIn, final CallbackInfo ci) {
 		CutelessMod.sendPacketsThisTick++;
+	}
+
+	@Inject(method = "exceptionCaught", at = @At("HEAD"), cancellable = true)
+	private void onExceptionCaught(final ChannelHandlerContext context, final Throwable throwable, final CallbackInfo ci) {
+		if (throwable instanceof DecoderException && "java.lang.IndexOutOfBoundsException: readerIndex(1) + length(1) exceeds writerIndex(1): PooledUnsafeDirectByteBuf(ridx: 1, widx: 1, cap: 1)".equals(throwable.getMessage())) {
+			ci.cancel();
+		}
 	}
 }
