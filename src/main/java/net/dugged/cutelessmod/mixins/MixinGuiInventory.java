f9dc2b6f9c46ab4d377732cf06e7dc35fcc8becb
nessie <nessie@hotmail.dk>
2021-04-08 21:59:19 +0200

    Center elytra-flying players in the inventory screen.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinGuiInventory.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinGuiInventory.java
@@ -1,6 +1,7 @@
 package net.dugged.cutelessmod.mixins;
 
 import net.dugged.cutelessmod.Reference;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiButtonImage;
 import net.minecraft.client.gui.GuiScreen;
@@ -15,19 +16,20 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiInventory.class)
 public abstract class MixinGuiInventory extends InventoryEffectRenderer {
 	@Unique
 	private static final ResourceLocation CUTELESSMOD_POTION_BUTTON = new ResourceLocation(Reference.MODID, "textures/potion_button.png");
+	@Unique
+	private boolean cutelessModShowPotionEffects = false;
 	@Shadow
 	private GuiButtonImage recipeButton;
 	@Shadow
 	@Final
 	private GuiRecipeBook recipeBookGui;
-	@Unique
-	private boolean cutelessModShowPotionEffects = false;
 
 	public MixinGuiInventory(final Container container) {
 		super(container);
@@ -54,5 +56,18 @@ public abstract class MixinGuiInventory extends InventoryEffectRenderer {
 			this.cutelessModShowPotionEffects = !this.cutelessModShowPotionEffects;
 			ci.cancel();
 		}
+	}
+
+	@ModifyVariable(method = "drawEntityOnScreen(IIIFFLnet/minecraft/entity/EntityLivingBase;)V", ordinal = 1, index = 1, name = "posY", at = @At("HEAD"), argsOnly = true)
+	private static int centreElytraFlyingPlayer(int value, final int posX, final int posY, final int scale) {
+		if (Minecraft.getMinecraft().player.isElytraFlying()) {
+			if (scale == 30) {
+				value -= 25;
+			} else if (scale == 20) {
+				value -= 16;
+			}
+		}
+
+		return value;
 	}
 }
