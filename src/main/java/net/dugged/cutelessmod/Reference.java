8826c4c0e1eef3578ad875225f24cbe9771cfa33
nessie <nessie@hotmail.dk>
2021-04-18 16:58:42 +0200

    Simplify assignment of isForge check.

--- src/main/java/net/dugged/cutelessmod/Reference.java
+++ src/main/java/net/dugged/cutelessmod/Reference.java
@@ -1,20 +1,17 @@
 package net.dugged.cutelessmod;
 
+import java.util.function.Supplier;
+
 public class Reference {
 	public static final String MODID = "@MODID@";
 	public static final String NAME = "@MODNAME@";
 	public static final String VERSION = "@VERSION@";
-	public static final boolean isForge;
-
-	static {
-		boolean isForgeTmp;
+	public static final boolean isForge = ((Supplier<Boolean>) () -> {
 		try {
 			Class.forName("net.minecraftforge.fml.client.FMLClientHandler");
-			isForgeTmp = true;
+			return true;
 		} catch (final ClassNotFoundException e) {
-			isForgeTmp = false;
+			return false;
 		}
-
-		isForge = isForgeTmp;
-	}
+	}).get();
 }
