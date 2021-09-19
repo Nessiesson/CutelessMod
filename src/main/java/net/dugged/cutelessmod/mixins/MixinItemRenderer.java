9a2cf4f565b927e9b07e0ec2dbba1b40bb9dd6c0
Jurassic-Fighter <72897587+Jurassic-Fighter@users.noreply.github.com>
2021-09-19 05:18:07 -0400

    Made showHandChangeAnimation more like 1.8 (#1)

--- src/main/java/net/dugged/cutelessmod/mixins/MixinItemRenderer.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinItemRenderer.java
@@ -1,25 +1,23 @@
 package net.dugged.cutelessmod.mixins;
 
 import net.dugged.cutelessmod.Configuration;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.util.EnumHandSide;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemRenderer.class)
 public abstract class MixinItemRenderer {
 
-	@Shadow
-	protected abstract void transformSideFirstPerson(EnumHandSide hand, float p_187459_2_);
 
-	@Redirect(method = "renderItemInFirstPerson(Lnet/minecraft/client/entity/AbstractClientPlayer;FFLnet/minecraft/util/EnumHand;FLnet/minecraft/item/ItemStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemRenderer;transformSideFirstPerson(Lnet/minecraft/util/EnumHandSide;F)V"))
-	public void disableItemSwapAnimation(ItemRenderer itemRenderer, EnumHandSide hand, float p_187459_2_) {
+	@Redirect(method = "updateEquippedItem()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/EntityPlayerSP;getCooledAttackStrength(F)F"))
+	private float disableItemSwapAnimation(EntityPlayerSP playerSP, float adjustTicks) {
 		if (!Configuration.showHandChangeAnimation) {
-			transformSideFirstPerson(hand, 0F);
+			return 1.0F;
 		} else {
-			transformSideFirstPerson(hand, p_187459_2_);
+			return playerSP.getCooledAttackStrength(adjustTicks);
 		}
+
 	}
 }
