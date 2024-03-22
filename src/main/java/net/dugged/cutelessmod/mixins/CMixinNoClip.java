b2f6f023c1b7c2372ec63b0b8aef49a81be9dff1
nessie <nessie@hotmail.dk>
2024-03-22 22:06:59 +0100

    Add super cool ignoreEntityWhenPlacing setting from carpet.

--- src/main/java/net/dugged/cutelessmod/mixins/CMixinNoClip.java
+++ src/main/java/net/dugged/cutelessmod/mixins/CMixinNoClip.java
@@ -1,16 +1,21 @@
 package net.dugged.cutelessmod.mixins;
 
 import net.dugged.cutelessmod.Configuration;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.MoverType;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.PlayerCapabilities;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import javax.annotation.Nullable;
@@ -34,7 +39,7 @@ public abstract class CMixinNoClip {
 		}
 
 		@Override
-		public void move(MoverType type, final double x, final double y, final double z) {
+		public void move(final MoverType type, final double x, final double y, final double z) {
 			if (type == MoverType.SELF || !(Configuration.noClip && this.isCreative() && this.capabilities.isFlying)) {
 				super.move(type, x, y, z);
 			}
@@ -46,6 +51,14 @@ public abstract class CMixinNoClip {
 		@Redirect(method = "renderWorldPass(IFJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/EntityPlayerSP;isSpectator()Z"))
 		private boolean fixSpectator(final EntityPlayerSP player) {
 			return player.isSpectator() || player.isCreative();
+		}
+	}
+
+	@Mixin(World.class)
+	public abstract static class MixinWorld {
+		@ModifyVariable(method = "mayPlace", at = @At("HEAD"), argsOnly = true)
+		private boolean ignoreEntityWhenPlacing(final boolean skipCollisionCheck, final Block block, final BlockPos pos, final boolean skip, final EnumFacing side, final @Nullable Entity placer) {
+			return Configuration.noClip && placer instanceof EntityPlayer && ((EntityPlayer) placer).isCreative() || skipCollisionCheck;
 		}
 	}
 }
