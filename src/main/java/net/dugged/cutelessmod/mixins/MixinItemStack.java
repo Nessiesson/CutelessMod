5e1d69a6bd0f5dfd5425e701d009ea7b54dd346c
nessie <nessie@hotmail.dk>
2024-07-18 18:15:24 +0200

    FINALLY fix usage of unparameterised enchantment sorting comparator.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinItemStack.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinItemStack.java
@@ -6,11 +6,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.text.translation.I18n;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
@@ -20,10 +22,17 @@ import java.util.Comparator;
 
 @Mixin(ItemStack.class)
 public abstract class MixinItemStack {
-	//TODO: Raw type
-	private final static Comparator comp = Comparator.<NBTTagCompound>comparingInt(t -> t.getShort("lvl"))
-			.reversed()
-			.thenComparingInt(t -> t.getShort("id"));
+	@Unique
+	private final static Comparator<NBTBase> cutelessmod$cmp = (o1, o2) -> {
+		if (o1 instanceof NBTTagCompound && o2 instanceof NBTTagCompound) {
+			return Comparator.<NBTTagCompound>comparingInt(t -> t.getShort("lvl"))
+					.reversed()
+					.thenComparingInt(t -> t.getShort("id"))
+					.compare((NBTTagCompound) o1, (NBTTagCompound) o2);
+		}
+
+		throw new RuntimeException("WTF somehow the tag list of your enchantment tag list was not a tag list."); // this never happens
+	};
 
 	@Shadow
 	public abstract NBTTagList getEnchantmentTagList();
@@ -41,7 +50,7 @@ public abstract class MixinItemStack {
 	private NBTTagList sortEnchantments(final ItemStack item) {
 		final NBTTagList list = this.getEnchantmentTagList();
 		if (Configuration.sortEnchantmentTooltip) {
-			((INBTTagList) list).getTagList().sort(comp);
+			((INBTTagList) list).getTagList().sort(cutelessmod$cmp);
 		}
 
 		return list;
