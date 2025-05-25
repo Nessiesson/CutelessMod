7788538baba60770afce434406b274107d2c9564
nessie <nessie@hotmail.dk>
2025-05-25 12:51:01 +0200

    Fix rainbow leaves turning red after a certain distance.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinRainbowLeafBlockColors.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinRainbowLeafBlockColors.java
@@ -15,18 +15,18 @@ import java.awt.*;
 @Mixin(targets = {"net/minecraft/client/renderer/color/BlockColors$4", "net/minecraft/client/renderer/color/BlockColors$5"})
 public abstract class MixinRainbowLeafBlockColors {
 	@Inject(method = "colorMultiplier(Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/util/math/BlockPos;I)I", at = @At("HEAD"), cancellable = true)
-	private void rainbowLeaves(IBlockState state, IBlockAccess world, BlockPos pos, int tintIndex, CallbackInfoReturnable<Integer> cir) {
+	private void rainbowLeaves(final IBlockState state, final IBlockAccess world, final BlockPos pos, final int tintIndex, final CallbackInfoReturnable<Integer> cir) {
 		if (!Configuration.showRainbowLeaves || pos == null) {
 			return;
 		}
 
 		final int sc = 1024;
-		final float hue = this.cutelessmodDist(pos.getX(), 32 * pos.getY(), pos.getX() + pos.getZ()) % sc / sc;
+		final float hue = this.cutelessmod$dist(pos.getX(), 32 * pos.getY(), pos.getX() + pos.getZ()) % sc / sc;
 		cir.setReturnValue(Color.HSBtoRGB(hue, 0.7F, 1F));
 	}
 
 	@Unique
-	private float cutelessmodDist(int x, int y, int z) {
+	private float cutelessmod$dist(final float x, final float y, final float z) {
 		return (float) Math.sqrt(x * x + y * y + z * z);
 	}
 }
