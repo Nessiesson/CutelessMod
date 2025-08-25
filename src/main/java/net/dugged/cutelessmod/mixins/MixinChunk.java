3284cbe602ef4744d48cee355fafb8c3bdf98b00
nessie <nessie@hotmail.dk>
2025-08-25 07:31:53 +0200

    ensure chunks will load in singleplayer.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinChunk.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinChunk.java
@@ -1,17 +1,24 @@
 package net.dugged.cutelessmod.mixins;
 
 import net.dugged.cutelessmod.Configuration;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Chunk.class)
 public abstract class MixinChunk {
+	@Shadow
+	@Final
+	private World world;
+
 	@Inject(method = "relightBlock", at = @At("HEAD"), cancellable = true)
 	private void noLightLagEverPlz(final int x, final int y, final int z, final CallbackInfo ci) {
-		if (!Configuration.lightUpdates) {
+		if (!Configuration.lightUpdates && this.world.isRemote) {
 			ci.cancel();
 		}
 	}
--- src/main/java/net/dugged/cutelessmod/mixins/MixinWorld.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinWorld.java
@@ -5,7 +5,9 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,12 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(World.class)
 public abstract class MixinWorld {
+	@Shadow
+	@Final
+	public boolean isRemote;
 	@Unique
 	private final GameSettings cutelessmod$settings = Minecraft.getMinecraft().gameSettings;
 
 	@Inject(method = "checkLightFor", at = @At("HEAD"), cancellable = true)
-	private void onLight(CallbackInfoReturnable<Boolean> cir) {
-		if (!Configuration.lightUpdates) {
+	private void cutelessmod$onLight(final CallbackInfoReturnable<Boolean> cir) {
+		if (!Configuration.lightUpdates && this.isRemote) {
 			cir.setReturnValue(false);
 		}
 	}
