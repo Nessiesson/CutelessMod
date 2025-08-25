0ac61f9d4315bf3f17cd8e71844af928fb466d0d
nessie <nessie@hotmail.dk>
2025-08-25 05:31:40 +0200

    Let you runClient without hard dependency on RenderLib.

--- src/main/java/net/dugged/cutelessmod/mixins/nothirium/MixinEntityRenderer.java
+++ src/main/java/net/dugged/cutelessmod/mixins/nothirium/MixinEntityRenderer.java
@@ -1,8 +1,6 @@
 package net.dugged.cutelessmod.mixins.nothirium;
 
-import meldexun.renderlib.renderer.entity.EntityRenderer;
 import net.dugged.cutelessmod.CutelessMod;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityMinecart;
@@ -13,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
-@Mixin(EntityRenderer.class)
+@Mixin(targets = "meldexun/renderlib/renderer/entity/EntityRenderer")
 public abstract class MixinEntityRenderer {
 	@Inject(method = "shouldRenderOutlines(Lnet/minecraft/entity/Entity;)Z", remap = false, at = @At("HEAD"), cancellable = true)
 	private void highlightAllEntitites(final Entity entity, final CallbackInfoReturnable<Boolean> cir) {
