9a1b813632b368d7af604951a0afe2cd9db8ea25
nessie <nessie@hotmail.dk>
2025-09-09 21:54:50 +0200

    Improved item stitcheroo code a little. No more off-by-one error and no redundant draw of the entire front/back of the item.

--- src/main/java/net/dugged/cutelessmod/mixins/MixinItemLayerModel.java
+++ src/main/java/net/dugged/cutelessmod/mixins/MixinItemLayerModel.java
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Optional;
 
-@SuppressWarnings({"DiscouragedInjectionPoint", "OptionalUsedAsFieldOrParameterType", "PointlessArithmeticExpression",})
+@SuppressWarnings({"DiscouragedInjectionPoint", "InvalidInjectorMethodSignature", "OptionalUsedAsFieldOrParameterType", "PointlessArithmeticExpression"})
 @Mixin(ItemLayerModel.class)
 public abstract class MixinItemLayerModel {
 	@Shadow
@@ -28,14 +28,17 @@ public abstract class MixinItemLayerModel {
 		throw new UnsupportedOperationException();
 	}
 
-	@SuppressWarnings("InvalidInjectorMethodSignature")
 	@Inject(method = "getQuadsForSprite", at = @At(value = "JUMP", opcode = Opcodes.GOTO, ordinal = 0), slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/util/EnumFacing;DOWN:Lnet/minecraft/util/EnumFacing;", opcode = Opcodes.GETSTATIC, ordinal = 0)), locals = LocalCapture.CAPTURE_FAILHARD)
 	private static void cutelessmod$fewerDots(final int tint, final TextureAtlasSprite sprite, final VertexFormat format, final Optional<TRSRTransformation> transform, final CallbackInfoReturnable<ImmutableList<BakedQuad>> cir,
-	                                          final ImmutableList.Builder<BakedQuad> builder, final int width, final int height, final @Coerce Object faceData, final boolean translucent, final int f, final int[] pixels,final boolean ptu, final boolean[] ptv, final int v, final int u, final int alpha, final boolean t) {
+	                                          final ImmutableList.Builder<BakedQuad> builder, final int width, final int height, final @Coerce Object faceData, final boolean translucent, final int f, final int[] pixels, final boolean ptu, final boolean[] ptv, final int v, int u, final int alpha, final boolean t) {
 		if (t || !Configuration.zzzSlightlyBetterItemStitchingMaybeKindaNotReally) {
 			return;
 		}
 
+		// we inject after a ++u so we have to correct for that.
+		--u;
+
+		// some smol number, chosen at random (trial/error idek)
 		final float eps = 1F / 4096F;
 
 		final float uMin = (1 - eps) * (u + 0F) / width;
@@ -63,5 +66,15 @@ public abstract class MixinItemLayerModel {
 				uMax, vMax, 8.5F / 16F, uMaxi, vMini,
 				uMin, vMax, 8.5F / 16F, uMini, vMini
 		));
+	}
+
+
+
+	@Inject(method = "getQuadsForSprite", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/model/ItemLayerModel;buildQuad(Lnet/minecraft/client/renderer/vertex/VertexFormat;Ljava/util/Optional;Lnet/minecraft/util/EnumFacing;Lnet/minecraft/client/renderer/texture/TextureAtlasSprite;IFFFFFFFFFFFFFFFFFFFF)Lnet/minecraft/client/renderer/block/model/BakedQuad;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+	private static void cutelessmod$doNotDrawTheseWhenStitching(final int tint, final TextureAtlasSprite sprite, final VertexFormat format, final Optional<TRSRTransformation> transform, final CallbackInfoReturnable<ImmutableList<BakedQuad>> cir,
+	                                                            final ImmutableList.Builder<BakedQuad> builder) {
+		if (Configuration.zzzSlightlyBetterItemStitchingMaybeKindaNotReally) {
+			cir.setReturnValue(builder.build());
+		}
 	}
 }
