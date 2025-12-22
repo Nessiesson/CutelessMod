28f3106c689ba617e37edf1f5c16ec5d5e67afb1
nessie <nessie@hotmail.dk>
2025-12-22 04:26:45 +0100

    fix partially committed x-ray.

--- src/main/java/net/dugged/cutelessmod/xray/XRay.java
+++ src/main/java/net/dugged/cutelessmod/xray/XRay.java
@@ -1,16 +1,19 @@
 package net.dugged.cutelessmod.xray;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHopper;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 
 public class XRay {
 	public boolean enabled;
 
-	public boolean isHiddenByXray(final Block block) {
+	public boolean isHiddenByXray(final IBlockState state) {
 		if (!this.enabled) {
 			return false;
 		}
 
-		return block == Blocks.STONE || block == Blocks.WATER || block == Blocks.FLOWING_WATER || block == Blocks.DIRT || block == Blocks.GRASS || block == Blocks.GRAVEL;
+		return !(state.getBlock() instanceof BlockHopper && state.getValue(BlockHopper.ENABLED));
+		//return state == Blocks.STONE || state == Blocks.WATER || state == Blocks.FLOWING_WATER || state == Blocks.DIRT || state == Blocks.GRASS || state == Blocks.GRAVEL;
 	}
 }
